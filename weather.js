301095831ac14ae33370f42c7446fadff37bd49d
Your Name <you@example.com>
2019-09-09 15:26:16 +0200

    added current temperature

--- weather.js
+++ weather.js
@@ -1,32 +1,26 @@
-
 (() => {
 
 //API key = 3e37be169bd64e9375bad5937587a45a
 // https://openweathermap.org/forecast5
 
 
-    document.getElementById("submitButton").addEventListener('click',function() {
+    document.getElementById("submitButton").addEventListener('click', function () {
 
         let city = document.getElementById("city-name").value;
 
-      //  +","+ countryCode  //after city
+        //  +","+ countryCode  //after city
 
-        fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city +"&units=metric" +
+        fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=metric" +
             '&appid=' + "3e37be169bd64e9375bad5937587a45a")
-            .then(function(response) {
+            .then(function (response) {
                 return response.json();
             })
-            .then(function(data) {
+            .then(function (data) {
                 console.log(data); //fetched information
                 //console.log(city); //city name value
 
 
-
-
-                //console.log(today.getDate());
-
-
-                for (var i= 0 ; i < 5 ; i++)  {    //loop over five days
+                for (var i = 0; i < 5; i++) {    //loop over five days
 
                     var iconId = data.list[i].weather[0].icon;
                     var temperatureMin = data.list[i].main.temp_min;
@@ -35,6 +29,7 @@
                     var today = new Date();
                     today.setDate(today.getDate() + i);
                     var displayDate = today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear();
+                    var currentTemp = data.list[i].main.temp;
 
 
                     //select template
@@ -44,10 +39,10 @@
 
 
                     clon.querySelector(".date").innerHTML = displayDate;
-                    clon.querySelector(".weatherImg").setAttribute("src", "http://openweathermap.org/img/wn/"+ iconId +"@2x.png");
-                    clon.querySelector(".temperature").innerHTML = temperatureMin +"°"+ "/ " + temperatureMax+ "°" ;
+                    clon.querySelector(".weatherImg").setAttribute("src", "http://openweathermap.org/img/wn/" + iconId + "@2x.png");
+                    clon.querySelector(".currentTemperature").innerHTML = currentTemp;
+                    clon.querySelector(".temperature").innerHTML = temperatureMin + "°" + "/ " + temperatureMax + "°";
                     clon.querySelector(".description").innerHTML = description;
-
 
 
                     // here append clone and therefore becomes element
